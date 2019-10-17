a7e14048268b388a769fba6a511c9f08fa98aed1
Tim Barr <TimothyEBarr@outlook.com>
2019-10-17 16:18:32 -0500

    Add project files.

---
+++ Eular Problem 1 - B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Eular_Problem_1___B
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Predicate<int> multipleOfThree = i => i % 3 == 0;
+            Predicate<int> multipleOfFive = i => i % 5 == 0;
+
+            for ( int x = 1; x < 1000; x++)
+            {
+                if ( multipleOfThree(x) )
+                    Console.WriteLine(x.ToString());
+                else if (multipleOfFive(x))
+                    Console.WriteLine(x.ToString());
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
