72b6da189d8473ee0b001b139dd075889f96a855
Tim Barr <TimothyEBarr@outlook.com>
2019-10-17 18:24:38 -0500

    Eular Problems beginning

---
+++ Eular Problem 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Eular_Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int x  = 0; x < 10; x++)
+            {
+                // THIS ONE DOES NOT FUNCTION YET
+                // Console.WriteLine(x);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
