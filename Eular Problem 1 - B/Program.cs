ba89f2eaa734054a0fa306c2666c986a1bcc05da
Tim Barr <TimothyEBarr@outlook.com>
2019-10-18 09:55:06 -0500

    Changed the MultipleOfThreeOrFive

--- Eular Problem 1 - B/Program.cs
+++ Eular Problem 1 - B/Program.cs
@@ -4,19 +4,15 @@ namespace Eular_Problem_1___B
 {
 
     /// <summary>
-    /// TEB:
-    /// TODO:  Add a description of the class
     /// 
-    /// This is the Program class.  It is a simple class that outputs to the console.
-    /// The program  produces a list of multiplies of 3 and 5 between 1 and below 1000.
+    ///
+    ///         If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+    ///         The sum of these multiples is 23.
     /// 
-    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-    /// The sum of these multiples is 23.
+    ///         Find the sum of all the multiples of 3 or 5 below 1000. 
     /// 
-    /// Find the sum of all the multiples of 3 or 5 below 1000. 
     /// 
-    /// TODO: Enhance the description of the class.
-    /// /// </summary>
+    /// </summary>
     class Program
     {
         /// <summary>
@@ -29,14 +25,23 @@ namespace Eular_Problem_1___B
             Predicate<int> multipleOfThree = i => i % 3 == 0;
             Predicate<int> multipleOfFive = i => i % 5 == 0;
 
-            for ( int x = 1; x < 1000; x++)
+            long sumOfNumbers = 0;
+
+            for ( int x = 0; x < 1000; x++)
             {
-                if ( multipleOfThree(x) )
-                    Console.WriteLine(x.ToString());
+                if (multipleOfThree(x))
+                {
+                    sumOfNumbers += x;
+//                    Console.WriteLine(x.ToString());
+                }
                 else if (multipleOfFive(x))
-                    Console.WriteLine(x.ToString());
+                {
+                    sumOfNumbers += x;
+//                    Console.WriteLine(x.ToString());
+                }
             }
 
+            Console.WriteLine("Total of factors: " + sumOfNumbers.ToString());
             Console.WriteLine("Multiplies Of Three And Five Problem 1");
         }
     }
